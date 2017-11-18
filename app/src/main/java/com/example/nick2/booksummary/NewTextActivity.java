4ea4f0113c57cc572efd16c87ea9aa781395271a
Nicolas Flores <31143150+nicolasjulioflores@users.noreply.github.com>
2017-11-18 18:29:24 -0500

    Saving text now supports small text; Now runs asynchronously so that user is not tied up in camera screen;

--- app/src/main/java/com/example/nick2/booksummary/NewTextActivity.java
+++ app/src/main/java/com/example/nick2/booksummary/NewTextActivity.java
@@ -10,12 +10,17 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,14 +34,21 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NewTextActivity extends AppCompatActivity {
     private static final String TAG = "NewTextActivity";
-
+    private static final String IMAGE_PATH = "imagePath";
 
 
     // Permission request codes need to be < 256
     private static final int RC_HANDLE_CAMERA_PERM = 2;
     private static final int REQUEST_IMAGE_CAPTURE = 25;
+
+    private String mCurrentImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +93,8 @@ public class NewTextActivity extends AppCompatActivity {
 
                 EditText TitleBox = findViewById(R.id.title);
 
-                if (TitleBox.getText().toString() == null) {
+                if (TitleBox.getText().toString().equals("")) {
+                    TitleBox.setText("Default Title");
                     // TODO:
                     // Show a snackbar or something to let them fill in their title
                     // They then should be able to proceed with saving
@@ -89,7 +102,7 @@ public class NewTextActivity extends AppCompatActivity {
 
                 // TODO:
                 // Save it and add it as a new view to MainActivity
-                // Once saved, this activity should finish and start a new editSavedTextActivity()
+                // Once saved, this activity should finish
 
             }
         });
@@ -111,89 +124,176 @@ public class NewTextActivity extends AppCompatActivity {
      * the constant.
      */
     @SuppressLint("InlinedApi")
-    private void processImage(Bitmap image) {
-        Context context = getApplicationContext();
+    private void processImage(final Bitmap image) {
+
 
         Log.d(TAG, "processImage called");
 
-        // A text recognizer is created to find text.  An associated multi-processor instance
-        // is set to receive the text recognition results, track the text, and maintain
-        // graphics for each text block on screen.  The factory is used by the multi-processor to
-        // create a separate tracker instance for each text block.
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+        new ProcessImageFilesTask().execute(image);
 
-        // Don't think i have to set a processor
-        // textRecognizer.setProcessor(new OcrDetectorProcessor(mGraphicOverlay));
-
-        if (!textRecognizer.isOperational()) {
-            // Note: The first time that an app using a Vision API is installed on a
-            // device, GMS will download a native libraries to the device in order to do detection.
-            // Usually this completes before the app is run for the first time.  But if that
-            // download has not yet completed, then the above call will not detect any text,
-            // barcodes, or faces.
-            //
-            // isOperational() can be used to check if the required native libraries are currently
-            // available.  The detectors will automatically become operational once the library
-            // downloads complete on device.
-            Log.w(TAG, "Detector dependencies are not yet available.");
-
-            // Check for low storage.  If there is low storage, the native library will not be
-            // downloaded, so detection will not become operational.
-            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-
-            if (hasLowStorage) {
-                Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
-                Log.w(TAG, getString(R.string.low_storage_error));
-            }
-
-            return;
-        }
-
-        Frame imageFrame = new Frame.Builder().setBitmap(image).build();
-
-        SparseArray<TextBlock> textBlock = textRecognizer.detect(imageFrame);
-        StringBuilder capturedString = new StringBuilder();
-        for (int i = 0; i < textBlock.size(); ++i) {
-            TextBlock item = textBlock.valueAt(i);
-            if (item != null && item.getValue() != null) {
-                //Log.d(TAG, "Text detected! " + item.getValue());
-
-                capturedString.append(item.getValue());
-
-
-            }
-        }
-        Log.d(TAG, capturedString.toString());
-
-        // If text was found then set it as the 'string'
-        EditText capturedStringBox = findViewById(R.id.capturedString);
-
-        String baseString = capturedStringBox.getText().toString();
-        baseString += capturedString.toString();
-        capturedStringBox.setText(baseString);
-
+        // Delete the file used
+        new File(mCurrentImagePath).delete(); // then delete it
 
     }
 
+    private class ProcessImageFilesTask extends AsyncTask<Bitmap, String, String> {
+        protected String doInBackground(Bitmap... image) {
+
+            Context context = getApplicationContext();
+
+            // A text recognizer is created to find text.  An associated multi-processor instance
+            // is set to receive the text recognition results, track the text, and maintain
+            // graphics for each text block on screen.  The factory is used by the multi-processor to
+            // create a separate tracker instance for each text block.
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+
+            // Don't think i have to set a processor
+            // textRecognizer.setProcessor(new OcrDetectorProcessor(mGraphicOverlay));
+
+            if (!textRecognizer.isOperational()) {
+                // Note: The first time that an app using a Vision API is installed on a
+                // device, GMS will download a native libraries to the device in order to do detection.
+                // Usually this completes before the app is run for the first time.  But if that
+                // download has not yet completed, then the above call will not detect any text,
+                // barcodes, or faces.
+                //
+                // isOperational() can be used to check if the required native libraries are currently
+                // available.  The detectors will automatically become operational once the library
+                // downloads complete on device.
+                Log.w(TAG, "Detector dependencies are not yet available.");
+
+                // Check for low storage.  If there is low storage, the native library will not be
+                // downloaded, so detection will not become operational.
+                IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+                boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+                    if (hasLowStorage) {
+                        Toast.makeText(getBaseContext(), R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                        Log.w(TAG, getString(R.string.low_storage_error));
+                    }
+
+                return null;
+            }
+
+            publishProgress("Processing String");
+
+            Frame imageFrame = new Frame.Builder().setBitmap(image[0]).build();
+
+            SparseArray<TextBlock> textBlock = textRecognizer.detect(imageFrame);
+            StringBuilder capturedString = new StringBuilder();
+            for (int i = 0; i < textBlock.size(); ++i) {
+                TextBlock item = textBlock.valueAt(i);
+                if (item != null && item.getValue() != null) {
+                    //Log.d(TAG, "Text detected! " + item.getValue());
+
+                    capturedString.append(item.getValue());
+
+
+                }
+            }
+
+
+
+            return capturedString.toString();
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            Toast.makeText(getBaseContext(),
+                    progress[0],
+                    Toast.LENGTH_SHORT ).show();
+        }
+
+        protected void onPostExecute(String capturedString) {
+            if (capturedString == null) {
+                Log.e(TAG, "No text to parse");
+                return;
+            }
+
+            Log.d(TAG, capturedString.toString());
+
+            // If text was found then set it as the 'string'
+            EditText capturedStringBox = findViewById(R.id.capturedString);
+
+            String baseString = capturedStringBox.getText().toString();
+            baseString += capturedString;
+            capturedStringBox.setText(baseString);
+            switchSaveButton();
+
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir =  new File(getBaseContext().getFilesDir(), "images");
+
+        if (!storageDir.exists()) {
+            if (!storageDir.mkdirs()) {
+                Log.e(TAG, "Error making the new storage directory");
+            }
+
+        }
+
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName, // prefix
+                    ".jpg", // suffix
+                    storageDir // directory
+            );
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+
+
     private void takePicture() {
-        // Calls the camera to 'Take a picture'; Request is received in onActivity Result
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(TAG, "Error creating photo file");
+            }
+
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getBaseContext(),
+                        "com.example.nick2.booksummary.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                mCurrentImagePath = photoFile.getAbsolutePath();
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
         }
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
 
-        // Retrieves the image from the camera and calls the function processImage() on it
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Log.d(TAG, "Received image");
-            Bundle extras = data.getExtras();
-            Bitmap image = (Bitmap) extras.get("data");
-            processImage(image);
+        Log.d(TAG, "onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            // Retrieves the image from the camera and calls the function processImage() on it
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "Received image");
+                Bundle extras = intent.getExtras();
+                processImage(BitmapFactory.decodeFile(mCurrentImagePath)); // process the image
+
+            } else {
+                Log.d(TAG, "Error in image capture, result code:" + Integer.toString(resultCode));
+            }
         }
 
     }
